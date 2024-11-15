734883edbe27d4dc6a44dbf8660ff7efdf4fe01a
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-11-16 02:31:46 +0600

    added W3D7

--- Week3/Day7/C_Ski_Resort.cpp
+++ Week3/Day7/C_Ski_Resort.cpp
@@ -1,7 +1,15 @@
-// https://codeforces.com/problemset/problem/1840/C
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
+
+int plus_factorial(int n)
+{
+    if (n <= 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return n + plus_factorial(n - 1);
+}
 
 int main()
 {
@@ -12,7 +20,30 @@ int main()
     cin >> t;
     while (t--)
     {
+        int n, k, q;
+        cin >> n >> k >> q;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        ll ans = 0;
+        ll cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] <= q)
+                cnt++;
+            else
+            {
+                if (cnt >= k)
+                    ans += (cnt * 1LL * (cnt + 1)) / 2 - ((k - 1) * cnt - ((k - 2) * (k - 1)) / 2);
+                cnt = 0;
+            }
         }
+        if (cnt >= k)
+            ans += (cnt * 1LL * (cnt + 1)) / 2 - ((k - 1) * cnt - ((k - 2) * (k - 1)) / 2);
+
+        cout << ans << '\n';
+    }
 
     return 0;
 }
--- Week3/Day7/Count_Occurences_of_Anagrams.cpp
+++ Week3/Day7/Count_Occurences_of_Anagrams.cpp
@@ -1,1 +1,35 @@
 // https://www.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
+
+class Solution
+{
+public:
+    int search(string pat, string txt)
+    {
+        int patLength = pat.length();
+        int txtLength = txt.length();
+
+        vector<int> patArray(26, 0);
+        vector<int> txtArray(26, 0);
+
+        for (int i = 0; i < patLength; i++)
+        {
+            patArray[pat[i] - 'a']++;
+            txtArray[txt[i] - 'a']++;
+        }
+
+        int count = 0;
+        if (patArray == txtArray)
+            count++;
+
+        for (int i = patLength; i < txtLength; i++)
+        {
+            txtArray[txt[i] - 'a']++;
+            txtArray[txt[i - patLength] - 'a']--;
+
+            if (patArray == txtArray)
+                count++;
+        }
+
+        return count;
+    }
+};
