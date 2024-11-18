7621e084e1c72304243c49a8fe6a63cf897bd323
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-11-18 09:55:52 +0600

    added W4D3

---
+++ Week4/Day2/A_Payment_Without_Change.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/1256/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, n, s;
+        cin >> a >> b >> n >> s;
+    }
+
+    return 0;
+}
