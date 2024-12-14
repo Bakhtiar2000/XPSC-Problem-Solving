82a136a71928ed1a05f5f5f473fd244e0c2cb292
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-12-15 00:59:09 +0600

    added W6D3, W6D4, W6D5, W6D6, W6D7

---
+++ Week6/Day4/B_Turtle_and_an_Infinite_Sequence.cpp
@@ -0,0 +1,30 @@
+// https://codeforces.com/contest/1981/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+
+        ll mn = max(0LL, n - m), x = n + m;
+        ll ans = mn;
+
+        while (ans < x)
+        {
+            ans = (ans | ans + 1);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ Week6/Day6/A_And_Then_There_Were_K.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/1527/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int x = __lg(n);
+        int ans = (1 << x) - 1;
+        cout << ans << endl;
+    }
+    return 0;
+}
