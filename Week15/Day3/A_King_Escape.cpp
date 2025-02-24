1ec09ebf97600fdb27ff8ab4397f6aebe18440c9
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2025-02-24 22:29:15 +0600

    added W15

---
+++ Week15/Day3/A_King_Escape.cpp
@@ -0,0 +1,44 @@
+// https://codeforces.com/problemset/problem/1033/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int a, b, c, d, x, y;
+    cin >> a >> b >> c >> d >> x >> y;
+
+    if (a > c && a < x)
+    {
+        cout << "NO" << endl;
+    }
+
+    else if (a < c && a > x)
+    {
+        cout << "NO" << endl;
+    }
+
+    else if (b > d && b < y)
+    {
+        cout << "NO" << endl;
+    }
+
+    else if (b < d && b > y)
+    {
+        cout << "NO" << endl;
+    }
+
+    else
+    {
+        cout << "YES" << endl;
+    }
+
+    return 0;
+}
